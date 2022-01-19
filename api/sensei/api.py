9e2a7fe1e67b5bb0da037b829035d91f084615cf
GoogleSheets <rtk3rns@gmail.com>
2022-01-19 08:00:07 -0500

    edit nodes and edges modifies and recreates the model

--- api/sensei/api.py
+++ api/sensei/api.py
@@ -340,8 +340,18 @@ def edit_nodes(model_id: str, payload: NodeParameter):
         break
 
     # Write the modfied model to file.
-    with create_and_open(model_filename, 'w') as filehandle:
-      json.dump(model, filehandle, indent=4, default=lambda obj: obj.__dict__)
+    #with create_and_open(model_filename, 'w') as filehandle:
+    #  json.dump(model, filehandle, indent=4, default=lambda obj: obj.__dict__)
+
+    try:
+      # Create the model with the updated node.
+      engine.create_model(
+        cag=ModelCreationRequest(id=model_id, nodes=model['nodes'], edges=model['edges']),
+        model_dirname=os.path.dirname(model_filename),
+      )
+    except Exception as e:
+      logger.error(f'ERROR:     Could not create model_id {model_id}')
+      return Response(status_code=500)
 
     return Response(status_code=200)
 
@@ -381,8 +391,19 @@ def edit_edges(model_id: str, payload: EditEdgesRequest):
           break
 
     # Write the modfiied model to file.
-    with create_and_open(model_filename, 'w') as filehandle:
-      json.dump(model, filehandle, indent=4, default=lambda obj: obj.__dict__)
+    #with create_and_open(model_filename, 'w') as filehandle:
+    #  json.dump(model, filehandle, indent=4, default=lambda obj: obj.__dict__)
+
+    try:
+      # Create the model with the updated edges.
+      engine.create_model(
+        cag=ModelCreationRequest(id=model_id, nodes=model['nodes'], edges=model['edges']),
+        model_dirname=os.path.dirname(model_filename),
+      )
+    except Exception as e:
+      logger.error(f'ERROR:     Could not create model_id {model_id}')
+      return Response(status_code=500)
+
 
     return EditEdgesResponse(status='success')
 
