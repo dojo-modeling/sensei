20fc405b509ea0d3253a27bc8b86cc49d50700cc
Ben Johnson <ben@jataware.com>
2022-02-15 23:25:38 +0000

    major updates

--- engine/sensei_engine/engine.py
+++ engine/sensei_engine/engine.py
@@ -1,195 +1,226 @@
-# import json
+#!/usr/bin/env python
+
+"""
+    engine.py
+"""
+
 import os
-import arrow
 import json
+import arrow
 import numpy as np
 import pandas as pd
-from datetime import datetime
-# import matplotlib.pyplot as plt
-# import networkx as nx
-# from itertools import cycle
+from joblib import dump, load
 
-from .engine_helpers import dyse_optimize, dyse_rollout
+import engine_helpers as h
 
 # --
-# Helpers
+# CREATE MODEL
 
-def get_ts(nodes, normalize=True):
-    df = []
-    for node in nodes:
-        k  = node['concept']
-        ts = [int(vv['timestamp']) for vv in node['values']]
-        va = [float(vv['value']) for vv in node['values']]
-
-        tmp = pd.DataFrame(index=ts, data=va, columns=[k])
-        df.append(tmp)
-    
-    df = pd.concat(df, axis=1).sort_index()
-
-    index_date = [arrow.get(xx // 1000).strftime('%Y-%m-%d') for xx in list(df.index)]
-    df['date'] = pd.to_datetime(pd.Series(index_date, index=df.index))
-    df         = df.set_index('date').apply(lambda x: x.asfreq(freq='M', method='ffill'))
-
-    if normalize:
-        print('!! NOT IMPLEMENTED')
-        # print('normalize')
-        # x              = df.values #returns a numpy array
-        # min_max_scaler = preprocessing.MinMaxScaler()
-        # x_scaled       = min_max_scaler.fit_transform(x)
-        # df             = pd.DataFrame(x_scaled, columns=df.columns, index=df.index)
-
-    return df.sort_index()
-
-
-def clean_ts(df_ts):
-    df_ts = df_ts.interpolate(limit_direction='both')
-    for c in df_ts.columns:
-        if df_ts[c].isnull().all():
-            df_ts[c] = 0
-
-    return df_ts
-
-def get_cag(edges):
-    cag = []
-    for edge in edges:
-        dst = edge['source']
-        src = edge['target']
-        cag.append({
-            'src'     : src,
-            'dst'     : dst,
-            'p_level' : 0.01,
-            'p_trend' : 0.01
-        })
-
-    return pd.DataFrame(cag)
-
-# --
-# API
-
-def create_model_output(payload, df_cag_opt):
+def create_model_output(nodes, df_cag, model):
     out = {
         "status" : "success",
         "nodes"  : [],
         "edges"  : [],
     }
 
-    for node in payload['nodes']:
+    for node in nodes:
         out['nodes'].append({
-            'concept'       : node['concept'],
+            'concept'       : node,
             'scalingFactor' : float(1),
             'scalingBias'   : float(0), # not sure how this is determined or why
         })
 
-    for edge in df_cag_opt.itertuples():
+    for edge in df_cag.itertuples():
+        
+        if edge.src == edge.dst:
+            coef  = 0.0 # TODO: Fix this -- we don't explicitly use self-loops
+        else:
+            coefs = model[edge.dst].get_regression_coefs()
+            coef  = float(coefs[coefs.regressor == edge.src].coefficient)
+        
         out['edges'].append({
             'source'  : edge.src,
             'target'  : edge.dst,
-            'weights' : [
-                str(edge.p_level),
-                str(edge.p_trend),
-            ]
+            'weights' : [str(coef), str(0)]
         })
 
     return out
 
-# --
-# API Hooks
 
 def create_model(cag, model_dirname):
     """
         this call takes CAG w/ everything inside
         outputs model w/ CAG sparsity pattern and optimized weights
     """
+    
+    # -
+    # get cag / training data
+    
+    df_cag   = h.parse_cag(cag['edges'])
+    df_model = h.parse_data(cag['nodes'])
+    nodes    = [node['concept'] for node in cag['nodes']]
+    periods  = {node['concept']:12 for node in cag['nodes']}
+    
+    progress_bar = h.ProgressBar(max_steps=len(nodes), fname=os.path.join(model_dirname, 'progress.json'))
+    
+    # -
+    # Preprocess data
 
-    with open(os.path.join(model_dirname, 'progress.json'), 'w') as f:
-        f.write(json.dumps({"progress" : 0}))
+    # min/max scale
+    scaler   = h.DataFrameScalar(nodes)
+    scaler   = scaler.fit(df_model)
+    df_model = scaler.transform(df_model)
+    
+    # interpolate regressors
+    df_model_interp = h.interpolate(df_model, df_cag, nodes, method='all')      # temporal interpolation
+    
+    nodes_interp    = [n for n in nodes if df_model_interp[n].isnull().all()] # graph interpolation
+    df_model_interp = h.interpolate(df_model_interp, df_cag, nodes_interp, method='neibs')
 
-    cag = cag.dict()
+    # -
+    # Fit model
+    
+    model = h.fit_model(df_model, df_model_interp, df_cag, nodes, periods, shift=True, progress_bar=progress_bar)
 
-    # time series data
-    df_ts = get_ts(cag['nodes'], normalize=False)
-    df_ts = clean_ts(df_ts)
-
-    # cag graph data
-    df_cag = get_cag(cag['edges'])
-
-    # run model
-    obs           = df_ts.copy()
-    data          = df_ts.copy()
-    data.iloc[1:] = np.nan # have to keep first row
-    df_cag_opt    = dyse_optimize(df_cag, data, obs)
-
-    # create output uncharted would like (to mimic old api calls)
-    output = create_model_output(cag, df_cag_opt)
-
-    # save
-    df_ts.to_csv(os.path.join(model_dirname, 'df_ts.csv'))
-    df_cag.to_csv(os.path.join(model_dirname, 'df_cag.csv'))
-    df_cag_opt.to_csv(os.path.join(model_dirname, 'df_cag_opt.csv'))
-
-    with open(os.path.join(model_dirname, 'create_model_output.json'), 'w') as f:
-        f.write(json.dumps(output))
+    # - 
+    # Serialize
+    
+    state = {
+        "df_cag"          : df_cag,
+        "df_model"        : df_model,
+        "df_model_interp" : df_model_interp,
+        "model"           : model,
+        "scaler"          : scaler,
+        "_meta"          : {
+            "nodes"     : nodes,
+            # "periods"   : periods,
+        }
+    }
+    dump(state, os.path.join(model_dirname, 'state.pkl'))
 
     with open(os.path.join(model_dirname, 'progress.json'), 'w') as f:
         f.write(json.dumps({"progress" : 100}))
-
-    return output
-
+    
+    # -
+    # Return
+    
+    api_result = create_model_output(nodes, df_cag, model)
+    return api_result
+    
 # --
+# INVOKE MODEL EXPERIMENT
 
-def make_empty_ts_df(start_time, end_time, time_steps_in_months, cols):
-    timesteps = arrow.Arrow.range('month', arrow.get(start_time), arrow.get(end_time))
-    timesteps = [ts.strftime('%Y-%m-%d') for ts in timesteps]
-    assert len(timesteps) == time_steps_in_months
-    return pd.DataFrame(np.nan, index=timesteps, columns=cols)
-
-
-def invoke_model_experiment_output(df_forecast_fut):
-    out_data = []
-    df_forecast_fut = df_forecast_fut.sort_index()
-    for c in df_forecast_fut.columns:
-        node = {
-            "concept" : c,
+def invoke_model_experiment_output(df_fut, all_preds):
+    out = []
+    _date_strs = df_fut._date_str
+    for node in all_preds.keys():
+        tmp = {
+            "concept" : node,
             "values"  : []
         }
-        for timestamp, value in df_forecast_fut[c].iteritems():
-            node['values'].append({
-                "timestamp" : int(arrow.get(timestamp).timestamp() * 1000),
-                "values"    : [value] * 100 # TODO: Need individual trajectories, this is just faking
+        
+        for i, _date_str in enumerate(_date_strs):
+            tmp['values'].append({
+                "timestamp" : int(arrow.get(_date_str).timestamp() * 1000),
+                "values"    : list(all_preds[node][i]),
             })
+        
+        out.append(tmp)
+    
+    return out
 
-        out_data.append(node)
 
-    return out_data
-
-
-def invoke_model_experiment(model_id, proj, model_dirname, experiment_filename):
-    proj        = proj.dict()
+def invoke_model_experiment(proj, model_dirname, experiment_filename):
+    
     proj_params = proj['experimentParam']
 
-    df_ts      = pd.read_csv(os.path.join(model_dirname, 'df_ts.csv')).set_index('date')
-    df_cag_opt = pd.read_csv(os.path.join(model_dirname, 'df_cag_opt.csv'))
-
-    df_ts_fut  = make_empty_ts_df(
-        start_time=proj_params['startTime'],
-        end_time=proj_params['endTime'],
-        time_steps_in_months=proj_params['numTimesteps'],
-        cols=df_ts.columns
+    state = load(os.path.join(model_dirname, 'state.pkl'))
+    
+    df_cag          = state['df_cag']
+    df_model        = state['df_model']
+    df_model_interp = state['df_model_interp']
+    scaler          = state['scaler']
+    model           = state['model']
+    nodes           = state['_meta']['nodes']
+    
+    # interpolate between end of training data and start of projection + create df_fut (w/ clamped values if neccessary)
+    df_fut = h.make_df_fut(
+        nodes         = nodes,
+        start_time    = proj_params['startTime'],
+        end_time      = proj_params['endTime'],
+        num_timesteps = proj_params['numTimesteps'],
+        last_time     = df_model._date_str.iloc[-1]
     )
 
-    # TODO: the dates don't lineup so need to create projection between known values and this df dates
-    df_ts_fut   = pd.concat([df_ts, df_ts_fut])
-    df_forecast = dyse_rollout(df_cag_opt, df_ts_fut)
+    df_fut = pd.concat([df_model_interp.tail(2), df_fut], ignore_index=True).copy()
     
-    # Format for output
+    # !! need to verify that these produce ~ the same results
+    df_fut, dist_fut = h.forecast(df_fut, model, nodes, df_cag)
+    
+    # drop "seed" data
+    df_fut = df_fut.tail(-2)
+    
+    # truncate to projection date range
     sel = (
-        (df_forecast.index >= arrow.get(proj_params['startTime']).strftime('%Y-%m-%d')) &
-        (df_forecast.index <= arrow.get(proj_params['endTime']).strftime('%Y-%m-%d'))
-    )
-    assert sel.sum() == proj_params['numTimesteps']
-    df_forecast_fut = df_forecast[sel]
+        (df_fut._date_str >= arrow.get(proj_params['startTime']).strftime('%Y-%m-%d')) &
+        (df_fut._date_str <= arrow.get(proj_params['endTime']).strftime('%Y-%m-%d'))
+    ).values
     
-    # Save
-    res = invoke_model_experiment_output(df_forecast_fut)
+    df_fut   = df_fut[sel]
+    dist_fut = {k:v[sel] for k,v in dist_fut.items()}
+    
+    # inverse_scale data
+    for node in nodes:
+        dist_fut[node] = scaler.scalers[node].inverse_transform(dist_fut[node])
+    
+    # json format
+    api_result = invoke_model_experiment_output(df_fut, dist_fut)
+    
+    # save
     with open(experiment_filename, 'w') as f:
-        f.write(json.dumps(res))
+        f.write(json.dumps(api_result))
+    
+    # >>
+    # SCRATCH: Plotting
+    # for node in api_result:
+    #     ts     = [xx['timestamp'] for xx in node['values']]
+    #     values = np.row_stack([xx['values'] for xx in node['values']]).T
+        
+    #     for xx in values:
+    #         _ = plt.plot(ts, xx, alpha=0.25, c='red')
+        
+    #     uval = df_model[node['concept']].values
+    #     uval = scaler.scalers[node['concept']].inverse_transform(uval[:,None]).squeeze()
+    #     _ = plt.plot(df_model.timestamp[-250:], uval[-250:], c='blue')
+    #     _ = plt.title(node['concept'])
+    #     show_plot(node['concept'].replace(' ', '_') + '-2.png')
+    # <<
+    
+    return api_result
+
+
+# --
+# CLI Test
+
+if __name__ == '__main__':
+    
+    model_dirname = 'data/models/'
+    
+    prob_id = '609aed2f' # SOI
+    # prob_id = '4510547d' # cheryl's cag
+    # prob_id = '334000c1' # pred/prey
+
+    root    = f'data/pam/{prob_id}'
+
+    # create model
+    model_dirname    = os.path.join(root, "model.json")
+    model            = json.load(open(model_dirname))
+    res_create_model = create_model(model, root)
+    print(res_create_model)
+
+    # invoke model expeirment / do projection
+    experiment_filename = os.path.join(root, 'test')
+    proj_path  = os.path.join(root, "projection.json")
+    proj       = h.fix_proj(json.load(open(proj_path)))
+    res_invoke_model = invoke_model_experiment(proj, root, experiment_filename)
+
--- engine/sensei_engine/engine_helpers.py
+++ engine/sensei_engine/engine_helpers.py
@@ -1,64 +1,264 @@
-import pandas as pd
+#!/usr/bin/env python
+
+"""
+  helpers.py
+"""
+
+import arrow
+import json
 import numpy as np
-from datetime import datetime
+import pandas as pd
 import networkx as nx
-from scipy.optimize import minimize
-from sklearn import preprocessing
+from tqdm import trange
+from collections import defaultdict
+from sklearn.preprocessing import MinMaxScaler
 
-def dyse_rollout(cag, data):
-    n_node     = len(data.columns)
-    key2idx    = {k:i for i,k in enumerate(data.columns)}
-    data_index = data.index
-    data       = data.values
+from orbit.models import DLT
 
-    # transition matrices
-    l_trans = np.zeros((n_node, n_node))
-    t_trans = np.zeros((n_node, n_node))
-    for e in cag.itertuples():
-        l_trans[key2idx[e.dst], key2idx[e.src]] = e.p_level
-        t_trans[key2idx[e.dst], key2idx[e.src]] = e.p_trend
-
-    # init + propagate
-    levels = data[0]
-    trends = np.zeros_like(data[0])
-    out    = [levels]
-    for i in range(1, len(data)):
-        new_levels = levels + l_trans @ levels + t_trans @ trends
-
-        sel = ~np.isnan(data[i])
-        new_levels[sel] = data[i, sel]
-
-        # clip 
-        #new_levels = new_levels.clip(min=0, max=n_levels)
-
-        # update state
-        trends = new_levels - levels
-        levels = new_levels
-        out.append(levels)
+class ProgressBar:
+  def __init__(self, fname, max_steps):
+    self.fname      = fname
+    self._step      = 0
+    self._max_steps = max_steps
     
-    out         = pd.DataFrame(out)
-    out.index   = data_index
-    out.columns = list(key2idx.keys())
+    open(self.fname, 'w').write(json.dumps({"progress" : 0}))
+  
+  def step(self):
+    self._step += 1
     
-    return out
- 
+    progress = round(100 * self._step / self._max_steps)
+    open(self.fname, 'w').write(json.dumps({"progress" : progress}))
 
-def dyse_optimize(cag, data, obs):
-    def _opt_wrapper(params, cag):
-        cag = cag.copy()
-        cag.p_level = params[:cag.shape[0]]
-        cag.p_trend = params[cag.shape[0]:]
-        pred = dyse_rollout(cag, data)
-        loss = ((pred.values - obs.values) ** 2).mean()
-        return loss
 
-    cag_opt = cag.copy()
-    cag_opt['p_level'] = np.nan
-    cag_opt['p_trend'] = np.nan
-    x0 = np.zeros(2 * cag.shape[0])
-    x  = minimize(_opt_wrapper, x0, args=(cag_opt,), options={'disp': True}).x
+def make_df_fut(nodes, start_time, end_time, num_timesteps, last_time):
+    """ 
+        currently only for time steps, 
+        need example with clamping to see format to add clamped value
+    """
+    
+    # timesteps for actual projection
+    proj_timesteps = arrow.Arrow.range('month', arrow.get(start_time), arrow.get(end_time))
+    proj_timesteps = [ts.strftime('%Y-%m-%d') for ts in proj_timesteps]
+    assert len(proj_timesteps) == num_timesteps
+    
+    # timesteps interpolating from last observed data to projection period. ?? is this necessary?
+    int_timesteps = arrow.Arrow.range('month', arrow.get(last_time).shift(months=1), arrow.get(start_time).shift(months=-1))
+    int_timesteps = [ts.strftime('%Y-%m-%d') for ts in int_timesteps]
+    
+    df_fut = pd.DataFrame(columns=nodes)
+    df_fut['_date_str'] = np.hstack([
+        int_timesteps,
+        proj_timesteps,
+    ])
+    df_fut['_date_str'] = pd.to_datetime(df_fut['_date_str'])
+    
+    return df_fut
 
-    cag_opt.p_level = x[:cag.shape[0]]
-    cag_opt.p_trend = x[cag.shape[0]:]
-    return cag_opt
+def parse_cag(edges):
+    """ convert model['edges'] to dataframe """
+    cag = []
+    for edge in edges:
+        src = edge['source']
+        dst = edge['target']
+        cag.append({
+            'src'     : src,
+            'dst'     : dst,
+            'p_level' : edge['polarity'] * 1,
+            'p_trend' : edge['polarity'] * 1,
+        })
+    
+    return pd.DataFrame(cag)
 
+
+def parse_data(nodes):
+    """ convert model['nodes'] to dataframe """
+    res = []
+    for node in nodes:
+        for xx in node['values']:
+            res.append({
+              "concept"   : node['concept'],
+              "timestamp" : xx['timestamp'],
+              "value"     : xx['value'] if 'value' in xx else np.mean(xx['values']),
+            })
+            if 'values' in xx and len(set(xx['values'])) > 1:
+                print('!!!')
+
+    res = pd.DataFrame(res)
+    res = res.pivot(index="timestamp", columns="concept")
+
+    res = res.value.reset_index()
+    res.columns.name = None
+
+    # add date fields
+    res['_date']     = res.timestamp.apply(lambda x: arrow.get(x / 1000))
+    res['_date_str'] = pd.to_datetime(res._date.apply(lambda x: x.strftime('%Y-%m-%d')))
+
+    return res
+
+def fix_proj(proj):
+  """ helper function for cleaning improperly formatted projection parameters """
+  
+  if 'experimentParams' in proj:
+    proj['experimentParam'] = proj.pop('experimentParams')
+  
+  proj['experimentParam']['numTimesteps'] = proj['experimentParam'].pop('numTimeSteps')
+  return proj
+
+
+class DataFrameScalar:
+    """ data frame scaling utility """
+    def __init__(self, nodes):
+        self.nodes   = nodes
+        self.scalers = {node:MinMaxScaler() for node in nodes}
+
+    def fit(self, df):
+        for node, scaler in self.scalers.items():
+            self.scalers[node] = scaler.fit(df[node].values[:,None])
+        return self
+
+    def transform(self, df):
+        df_scaled = df.copy()
+        for node, scaler in self.scalers.items():
+            df_scaled[node] = scaler.transform(df_scaled[node].values[:,None]).squeeze()
+        return df_scaled
+
+    def inverse_transform(self, df_scaled):
+        df_unscaled = df_scaled.copy()
+        for node, scaler in self.scalers.items():
+            df_unscaled[node] = scaler.inverse_transform(df_unscaled[node].values[:,None]).squeeze()
+        return df_unscaled
+
+
+def interpolate(df, df_cag, nodes, method='all'):
+    assert method in ['all', 'neibs']
+    
+    df_interp = df.copy()
+    if method == 'all': # linear between obs, constant forward / backward
+        for node in nodes:
+            df_interp[node] = df_interp[node].interpolate(method='linear').bfill().ffill() 
+
+    elif method == 'neibs': # "interpolate" w/ data from neighbors, using simple weighted sum
+        for node in nodes:
+            in_edges        = df_cag[df_cag.dst == node]
+            df_interp[node] = df_interp[in_edges.src].values @ in_edges.p_value.values
+        
+    return df_interp
+
+
+def make_graph_input(df, df_interp, df_cag, node, shift=True):
+  regressors         = list(set(df_cag.src[df_cag.dst == node]))
+  regressors         = [r for r in regressors if r != node]
+  df_reg             = df_interp[['_date_str', node] + regressors].copy() # use interpolated data for regressors
+  df_reg[node]       = df[node]                                           # use real data for target
+  
+  if shift:
+    df_reg[regressors] = df_reg[regressors].shift(1)                      # use one-step-old regressors - easiest way to handle loops
+    df_reg             = df_reg.tail(-1)
+  
+  return df_reg, regressors
+
+
+def fit_model(df_train, df_train_interp, df_cag, nodes, periods, shift=True, progress_bar=None):
+  models = {}
+  for node in nodes:
+        
+    # make input
+    df_reg, regressors = make_graph_input(df_train, df_train_interp, df_cag, node, shift=shift)
+    
+    # clean target
+    if df_reg[node].isnull().all():
+      df_reg[node] = df_train_interp[node].copy()     # if all target missing, use "neighbor interpolated version"
+    else:
+      idx0   = np.where(df_reg[node].notnull())[0][0] # else, truncate to after first non-null observation
+      df_reg = df_reg[idx0:]
+    
+    # fit model
+    dlt_params = {
+        "response_col"           : node, 
+        "regressor_col"          : regressors,
+        "date_col"               : '_date_str',
+        "estimator"              : 'stan-map',
+        "n_bootstrap_draws"      : 200,
+        "seed"                   : 123,
+        "verbose"                : False,
+        "prediction_percentiles" : list(np.arange(10, 95, 5).astype(int)),
+    }
+    
+    try:
+      # try w/ correct seasonality .. but sometimes this fails if there's too much missing data...
+      orbit_model  = DLT(seasonality=periods[node], **dlt_params)
+      models[node] = orbit_model.fit(df=df_reg.tail(1000)) # fit on most recent 1K samples
+    except:
+      try:
+        # if it fails, ignore the seasonality
+        print(f'fit_model: error at {node} -- seasonality fail, retrying') # !! How can we work around this
+        
+        orbit_model  = DLT(**dlt_params)
+        models[node] = orbit_model.fit(df=df_reg.tail(1000))
+      except:
+        print(f'fit_model: error at {node} -- complete fail, skipping') # !! How can we work around this
+    
+    if progress_bar is not None:
+      progress_bar.step()
+
+  return models
+
+def forecast(df_fut, model, nodes, df_cag):
+  g = nx.from_pandas_edgelist(df_cag, source='src', target='dst', create_using=nx.DiGraph())
+  if nx.is_directed_acyclic_graph(g):
+      return _forecast_topology(df_fut, model, list(nx.topological_sort(g)), df_cag)
+  else:
+      return _forecast_stepwise(df_fut, model, nodes, df_cag)
+
+
+def _forecast_stepwise(df_fut, model, nodes, df_cag):
+  # !! need to handle clamps
+  
+  dist_fut = defaultdict(lambda: [])
+  for idx in trange(2, df_fut.shape[0]):
+    for node in nodes:
+      
+      df_reg, _ = make_graph_input(df_fut.iloc[:idx + 1], df_fut.iloc[:idx + 1], df_cag, node) # !! efficiency
+      
+      if node in model:
+        df_pred = model[node].predict(df=df_reg)
+        
+        curr_pred      = df_pred.tail(1)
+        curr_pred_med  = float(curr_pred.prediction)
+        curr_pred_dist = curr_pred[[c for c in curr_pred.columns if 'prediction' in c]].values
+        
+        df_fut[node].iloc[idx] = curr_pred_med
+        dist_fut[node].append(curr_pred_dist)
+        
+      else:
+        # !! failed to train a model -- fall back to just carrying forward values w/ no variance.
+        val                    = float(df_reg[node][df_reg[node].notnull()].iloc[-1])
+        df_fut[node].iloc[idx] = val
+        dist_fut[node].append(np.ones(17) * val) # number of percentiles
+
+  dist_fut = {k:np.row_stack(v) for k,v in dist_fut.items()}
+  return df_fut, dist_fut
+
+
+def _forecast_topology(df_fut, model, nodes, df_cag):
+  # !! need to handle clamps
+  
+  dist_fut = {}
+  for node in nodes:
+    df_reg, _ = make_graph_input(df_fut, df_fut, df_cag, node)
+    
+    if node in model:
+      df_pred   = model[node].predict(df=df_reg)
+      
+      df_pred   = df_pred.tail(-1)
+      pred_med  = df_pred.prediction.values
+      pred_dist = df_pred[[c for c in df_pred.columns if 'prediction' in c]].values
+      
+      df_fut[node].iloc[2:] = pred_med
+      dist_fut[node] = pred_dist
+    else:
+      val            = float(df_reg[node][df_reg[node].notnull()].iloc[-1])
+      df_fut[node]   = val
+      dist_fut[node] = np.ones((df_fut.shape[0], 17)) * val
+  
+  return df_fut, dist_fut
