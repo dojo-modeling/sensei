db8e368983a04825283bb5d2c702140465502a99
Ben Johnson <ben@jataware.com>
2022-03-01 18:36:35 +0000

    [FEATURE] adding compile-trigger to keep first call from taking a long time

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,9 @@ WORKDIR /opt/sensei-api
 
 RUN pip install -r /opt/sensei-api/requirements.txt
 COPY api /opt/sensei-api
+COPY tests /opt/sensei-tests
 
 EXPOSE 8088
 
+RUN "python /opt/sensei-tests/compile-trigger.py"
 CMD ["uvicorn", "sensei.api:app", "--host", "0.0.0.0", "--port", "8088", "--log-config", "logging.yaml"]
---
+++ tests/compile-trigger.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""
+   compile-trigger.py
+"""
+
+import arrow
+import numpy as np
+import pandas as pd
+from orbit.models import DLT
+
+_date_str = [d.strftime('%Y-%m-%d') for d in arrow.Arrow.range(
+  frame='month',
+  start=arrow.get('2021-01-01'),
+  end=arrow.get('2021-12-01'),
+)]
+
+df = pd.DataFrame({
+  "x0"        : np.random.uniform(12),
+  "x1"        : np.random.uniform(12),
+  "y"         : np.random.uniform(12),
+  "_date_str" : _date_str
+})
+
+
+dlt_params = {
+    "response_col"           : "y", 
+    "regressor_col"          : ["x0", "x1"],
+    "date_col"               : '_date_str',
+    "estimator"              : 'stan-map',
+    "n_bootstrap_draws"      : 200,
+    "seed"                   : 123,
+    "verbose"                : False,
+    "prediction_percentiles" : list(np.arange(5, 95, 1).astype(int)),
+}
+
+model = DLT(seasonality=6, **dlt_params)
+model = model.fit(df=df)
+_ = model.predict(df)
